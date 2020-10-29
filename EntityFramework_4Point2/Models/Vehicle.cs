3d1cdc71ae1e0582d26ac34b7dcb530ad2129bc1
James Grieve <jamesonrgrieve@gmail.com>
2020-10-29 10:40:33 -0600

    Scaffold generation;

---
+++ EntityFramework_4Point2/Models/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntityFramework_4Point2.Models
+{
+    [Table("vehicle")]
+    public partial class Vehicle
+    {
+        [Key]
+        [Column("ID", TypeName = "int(10)")]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(25)")]
+        public string Manufacturer { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(30)")]
+        public string Model { get; set; }
+        [Column(TypeName = "int(10)")]
+        public int ModelYear { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(25)")]
+        public string Colour { get; set; }
+    }
+}
---
+++ EntityFramework_4Point2/Models/VehicleContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EntityFramework_4Point2.Models
+{
+    public partial class VehicleContext : DbContext
+    {
+        public VehicleContext()
+        {
+        }
+
+        public VehicleContext(DbContextOptions<VehicleContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Vehicle> Vehicle { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("server=localhost;port=3306;user=root;database=dbfirst_4point2", x => x.ServerVersion("10.4.14-mariadb"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Vehicle>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Colour)
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_general_ci");
+
+                entity.Property(e => e.Manufacturer)
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_general_ci");
+
+                entity.Property(e => e.Model)
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_general_ci");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
