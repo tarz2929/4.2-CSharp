9b2945401c3ba27dc44efa09c8dead9f2bf53e34
James Grieve <jamesonrgrieve@gmail.com>
2020-10-30 09:38:02 -0600

    Declare priamry key;

--- EntityFramework_4Point2/Models/Vehicle.cs
+++ EntityFramework_4Point2/Models/Vehicle.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace EntityFramework_4Point2.Models
 {
+    [Table("vehicle")]
     class Vehicle
     {
+        [Key]
+        [Column(TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+
+
     }
 }
--- EntityFramework_4Point2/Program.cs
+++ EntityFramework_4Point2/Program.cs
@@ -6,8 +6,13 @@ namespace EntityFramework_4Point2
     {
         static void Main(string[] args)
         {
-
-            
+            /*
+            Step 1: Create Models folder.
+            Step 2: Create context class, and any required model classes.
+            Step 3: Per Class:
+                Step 3a: Name the table for the model class.
+                Step 3b: Declare primary key property.
+            */
         }
     }
 }
